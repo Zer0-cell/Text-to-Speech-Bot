e80a2f278bbf565c2313e489b5e0eb3de913f583
Zer0 <shamir.hossain18@gmail.com>
2024-09-29 21:12:33 +0600

    .

--- Robo Speaker/main.py
+++ Robo Speaker/main.py
@@ -9,15 +9,19 @@ def speak(text):
     engine.runAndWait()
 
 if __name__ == "__main__":
-    print("Welcome to RoboSpeaker 1.1 created by Zero")
-
+    intro = "Welcome to text to speech RoboSpeaker 1.1 created by Zero"
+    print(intro)
+    speak(intro)
+    outro = "Bye! Have a great day."
+    
     while True:
         # Ask the user for input
         text = input("Enter the text you want me to say (Press 'q' to quit): ")
 
         # Exit if the user enters 'q'
         if text.lower() == "q":
-            print("Goodbye!")
+            print(outro)
+            speak(outro)
             break
 
         # Speak the text
